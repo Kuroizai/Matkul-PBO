e6fb3cb571bf06c1e9153b607c6e00f90b8aa043
nuwbie-11 <63080447+nuwbie-11@users.noreply.github.com>
2021-03-01 10:53:02 +0700

    Update Pert 1
    Content
    -Applicated if name==main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import wx
 
-app = wx.App()
-
-#Frame
-
-frm = wx.Frame(None,title='Pert 1')
-
-frm.Show()
 
 
 
-app.MainLoop()
+
+
+if __name__=='__main__':
+    app = wx.App()
+    #Frame
+    frm = wx.Frame(None,title='Pert 1')
+    frm.Show()
+    app.MainLoop()
