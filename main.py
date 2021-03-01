20083a285170e78745ee6df9f65d89d5e8d7d1df
nuwbie-11 <63080447+nuwbie-11@users.noreply.github.com>
2021-03-01 11:25:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wx
+from wx.core import Size
 
 
 
@@ -8,6 +9,8 @@ import wx
 if __name__=='__main__':
     app = wx.App()
     #Frame
-    frm = wx.Frame(None,title='Pert 1')
+    frm = wx.Frame(None,title='Pert 1',size = (400,400))
+    panel=wx.Panel(frm)
+    label = wx.StaticText(panel,label='Hello',pos=(170,100)) #Static (Absolute)
     frm.Show()
     app.MainLoop()
